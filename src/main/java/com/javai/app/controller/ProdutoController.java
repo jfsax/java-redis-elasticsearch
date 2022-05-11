02dd26f6c3996de919d7cdcfaf55025488a85543
jfsax <jessicafsax@gmail.com>
2022-05-10 21:31:39 -0300

    fix: remoção de imports

--- src/main/java/com/javai/app/controller/ProdutoController.java
+++ src/main/java/com/javai/app/controller/ProdutoController.java
@@ -3,7 +3,6 @@ package com.javai.app.controller;
 import com.javai.app.dao.ProdutoDAO;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 
 public class ProdutoController {
     @Autowired
